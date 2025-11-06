54fb9c2ab5aeb7939f606cceb23b58b1885fd8af
praseeda11249a111 <11249a111@kanchiuniv.ac.in>
2025-11-05 21:40:58 -0800

    Add files via upload

---
+++ functionargument.c
@@ -0,0 +1,41 @@
+#include<iostream>
+unsing namespace std;
+class Time
+{
+    private:
+    int hours,minutes;
+    public:
+    void gettime();
+    void addtime(Time t1,Time t2);
+    void display();
+};
+void Time::gettime()
+{
+    cout<<"enter the hours:";
+    cin>>hours;
+    cout<<"enter the minutes:";
+    cin>>minutes;
+}
+void Time::addtime(Time t1,Time t2)
+{
+    minutes=(t1.minutes+t2.minutes);
+    hours=minutes/60;
+    minutes=minutes%60;
+    hours=hours+t1.hours+t2.hours;
+}
+void Time::g.display()
+{
+    cout<<"hours"<<hours;
+    cout<<"minutes"<<minutes;
+}
+int main()
+{
+    Time t1,t2,t3;
+    t1.gettime();
+    t2.gettime()
+    t3.addtime();
+    t1.display();
+    t2.display();
+    t3.display();
+    return 0;
+}
