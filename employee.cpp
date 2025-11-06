ac3b1cc063239969e8aae7ebf2405c64444a5357
praseeda11249a111 <11249a111@kanchiuniv.ac.in>
2025-11-05 21:41:39 -0800

    Add files via upload

---
+++ employee.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include<iomanip>
+using namespace std;
+class employee
+{
+    private:
+    int id ;
+    string name ;
+    float salary;
+    public:
+    void getdetails(int i){
+
+  cout<<"enter details for employee"<<i<<":\n";
+        cout<<"ID:";
+        cin>>id;
+        cout<<"name:";
+        cin>>name;
+        cout<<"salary:";
+        cin>>salary;
+      cout<<endl;
+    }
+    
+          void display()
+        {
+           cout<<left<<setw(10)<<id<<left<<setw(15)<<name<<left<<setw(10)<<salary<<endl;        
+        }
+};
+    int main()
+    {
+       int n;
+       cout <<"enter number of employess:";
+       cin>>n;
+       employee emp[n];
+       for (int i=0;i<n;i++)
+       {
+       emp[i].getdetails(i+1);
+       }
+      cout<<"n_ _ _ _ _ _ _ _ _ _ _ \n";
+      cout<<left<<setw(10)<<"ID"<<left<<setw(15)<<"name"<<left<<setw(10)<<"salary"<<endl;
+      for (int i=0;i<n;i++)
+      {  
+        emp[i]. display();
+      }
+        cout<<"_ _ _ _ _ _\n";
+        return 0 ;
+    }
